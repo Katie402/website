761f2228ab35868ee799dfb3b58e7379bc0219ef
katie <park402da@gmail.com>
2020-11-04 18:43:54 +0900

    Modified index.html

--- js/ui.js
+++ js/ui.js
@@ -279,6 +279,7 @@ $(function(){
                 //slick section04
 
                 var $imgSlider = $('#sec04 .img_slider');
+                var $txtSlider = $('#sec04 .me_desc');
 
                 $( document ).ready(function() {
                     $imgSlider.slick({
@@ -290,62 +291,75 @@ $(function(){
                         cssEase: 'linear',
                         infinite: true,
                     });
+
+                    $txtSlider.slick({
+                        autoplay: true,
+                        // autoplaySpeed: 4000,
+                        // speed: 400,
+                        arrows: false,
+                        vertical: true,
+                        // verticalScrolling: true,
+                        cssEase: 'linear',
+                        infinite: true,
+                    });
+
                 });
 
-                var words = document.getElementsByClassName('word');
-                var wordArray = [];
-                var currentWord = 0;
+
+                // var words = document.getElementsByClassName('word');
+                // var wordArray = [];
+                // var currentWord = 0;
         
-                words[currentWord].style.opacity = 1;
-                for (var i = 0; i < words.length; i++) {
-                splitLetters(words[i]);
-                }
+                // words[currentWord].style.opacity = 1;
+                // for (var i = 0; i < words.length; i++) {
+                // splitLetters(words[i]);
+                // }
         
-                function changeWord() {
-                var cw = wordArray[currentWord];
-                var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-                for (var i = 0; i < cw.length; i++) {
-                    animateLetterOut(cw, i);
-                }
+                // function changeWord() {
+                // var cw = wordArray[currentWord];
+                // var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+                // for (var i = 0; i < cw.length; i++) {
+                //     animateLetterOut(cw, i);
+                // }
                 
-                for (var i = 0; i < nw.length; i++) {
-                    nw[i].className = 'letter behind';
-                    nw[0].parentElement.style.opacity = 1;
-                    animateLetterIn(nw, i);
-                }
+                // for (var i = 0; i < nw.length; i++) {
+                //     nw[i].className = 'letter behind';
+                //     nw[0].parentElement.style.opacity = 1;
+                //     animateLetterIn(nw, i);
+                // }
                 
-                currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-                }
+                // currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+                // }
         
-                function animateLetterOut(cw, i) {
-                setTimeout(function() {
-                        cw[i].className = 'letter out';
-                }, i*80);
-                }
+                // function animateLetterOut(cw, i) {
+                // setTimeout(function() {
+                //         cw[i].className = 'letter out';
+                // }, i*80);
+                // }
         
-                function animateLetterIn(nw, i) {
-                setTimeout(function() {
-                        nw[i].className = 'letter in';
-                }, 340+(i*80));
-                }
+                // function animateLetterIn(nw, i) {
+                // setTimeout(function() {
+                //         nw[i].className = 'letter in';
+                // }, 340+(i*80));
+                // }
         
-                function splitLetters(word) {
-                var content = word.innerHTML;
-                word.innerHTML = '';
-                var letters = [];
-                for (var i = 0; i < content.length; i++) {
-                    var letter = document.createElement('span');
-                    letter.className = 'letter';
-                    letter.innerHTML = content.charAt(i);
-                    word.appendChild(letter);
-                    letters.push(letter);
-                }
+                // function splitLetters(word) {
+                // var content = word.innerHTML;
+                // word.innerHTML = '';
+                // var letters = [];
+                // for (var i = 0; i < content.length; i++) {
+                //     var letter = document.createElement('span');
+                //     letter.className = 'letter';
+                //     letter.innerHTML = content.charAt(i);
+                //     word.appendChild(letter);
+                //     letters.push(letter);
+                // }
                 
-                wordArray.push(letters);
-                }
+                // wordArray.push(letters);
+                // }
         
-                changeWord();
-                setInterval(changeWord, 4000);
+                // changeWord();
+                // setInterval(changeWord, 4000);
 
             }); 
         
